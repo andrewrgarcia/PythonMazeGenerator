8848dc1b072acc6807b4f1333d8ee853c9999827
Andrew Garcia <garcia.gtr@gmail.com>
2020-03-01 20:22:19 -0500

    changes

--- maze.py
+++ maze.py
@@ -14,17 +14,18 @@ import numpy as np
 import timeit
 
 
-random.seed(666)
 
 def make():
+    random.seed(666)
+
     start = timeit.default_timer()
     # set up pygame window
     WIDTH = 450
     HEIGHT = 450
     FPS = 30
-    
+
     maze_len = 20
-    
+
     # Define colours
     WHITE = (255, 255, 255)
     GREEN = (0, 255, 0,)
@@ -33,14 +34,14 @@ def make():
         xc,yc = random.randint(0,100),random.randint(0,100)
         return (xc,yc,255)
     YELLOW = (255 ,255 ,0)
-    
+
     # initalise Pygame
     pygame.init()
     pygame.mixer.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Maze PC performance test")
     clock = pygame.time.Clock()
-    
+
     # setup maze variables
     x = 0                    # x axis
     y = 0                    # y axis
@@ -49,8 +50,8 @@ def make():
     visited = []
     stack = []
     solution = {}
-    
-    
+
+
     # build the grid
     def build_grid(x, y, w):
         for i in range(1,maze_len+1):
@@ -63,43 +64,43 @@ def make():
                 pygame.draw.line(screen, WHITE, [x, y + w], [x, y])           # left of cell
                 grid.append((x,y))                                            # add cell to grid list
                 x = x + maze_len                                                    # move cell to new position
-    
-    
+
+
     def push_up(x, y):
         pygame.draw.rect(screen,blue(), (x + 1, y - w + 1, maze_len-1, 2*maze_len-1), 0)         # draw a rectangle twice the width of the cell
         pygame.display.update()                                              # to animate the wall being removed
-    
-    
+
+
     def push_down(x, y):
         pygame.draw.rect(screen, blue(), (x +  1, y + 1, maze_len-1, 2*maze_len-1), 0)
         pygame.display.update()
-    
-    
+
+
     def push_left(x, y):
         pygame.draw.rect(screen, blue(), (x - w +1, y +1, 2*maze_len-1, maze_len-1), 0)
         pygame.display.update()
-    
-    
+
+
     def push_right(x, y):
         pygame.draw.rect(screen, blue(), (x +1, y +1, 2*maze_len-1, maze_len-1), 0)
         pygame.display.update()
-    
-    
+
+
     def single_cell( x, y):
         pygame.draw.rect(screen, GREEN, (x +1, y +1, maze_len-2, maze_len-2), 0)          # draw a single width cell
         pygame.display.update()
-    
-    
+
+
     def backtracking_cell(x, y):
         pygame.draw.rect(screen, blue(), (x +1, y +1, maze_len-2, maze_len-2), 0)        # used to re-colour the path after single_cell
         pygame.display.update()                                        # has visited cell
-    
-    
+
+
     def solution_cell(x,y):
         pygame.draw.rect(screen, YELLOW, (x+8, y+8, 5, 5), 0)             # used to show the solution
         pygame.display.update()                                        # has visited cell
-    
-    
+
+
     def carve_out_maze(x,y):
         single_cell(x, y)                                              # starting positing of maze
         stack.append((x,y))                                            # place starting cell into stack
@@ -109,40 +110,40 @@ def make():
             cell = []                                                  # define cell list
             if (x + w, y) not in visited and (x + w, y) in grid:       # right cell available?
                 cell.append("right")                                   # if yes add to cell list
-    
+
             if (x - w, y) not in visited and (x - w, y) in grid:       # left cell available?
                 cell.append("left")
-    
+
             if (x , y + w) not in visited and (x , y + w) in grid:     # down cell available?
                 cell.append("down")
-    
+
             if (x, y - w) not in visited and (x , y - w) in grid:      # up cell available?
                 cell.append("up")
-    
+
             if len(cell) > 0:                                          # check to see if cell list is empty
                 cell_chosen = (random.choice(cell))                    # select one of the cell randomly
-    
+
                 if cell_chosen == "right":                             # if this cell has been chosen
                     push_right(x, y)                                   # call push_right function
                     solution[(x + w, y)] = x, y                        # solution = dictionary key = new cell, other = current cell
                     x = x + w                                          # make this cell the current cell
                     visited.append((x, y))                              # add to visited list
                     stack.append((x, y))                                # place current cell on to stack
-    
+
                 elif cell_chosen == "left":
                     push_left(x, y)
                     solution[(x - w, y)] = x, y
                     x = x - w
                     visited.append((x, y))
                     stack.append((x, y))
-    
+
                 elif cell_chosen == "down":
                     push_down(x, y)
                     solution[(x , y + w)] = x, y
                     y = y + w
                     visited.append((x, y))
                     stack.append((x, y))
-    
+
                 elif cell_chosen == "up":
                     push_up(x, y)
                     solution[(x , y - w)] = x, y
@@ -154,24 +155,25 @@ def make():
                 single_cell(x, y)                                     # use single_cell function to show backtracking image
     #            time.sleep(.05)                                       # slow program down a bit
                 backtracking_cell(x, y)                               # change colour to green to identify backtracking path
-    
-    
+
+
     def plot_route_back(x,y):
         solution_cell(x, y)                                          # solution list contains all the coordinates to route back to start
         while (x, y) != (maze_len,maze_len):                                     # loop until cell position == start position
             x, y = solution[x, y]                                    # "key value" now becomes the new key
             solution_cell(x, y)                                      # animate route back
     #        time.sleep(.1)
-    
-    
+
+
     x, y = maze_len, maze_len                     # starting position of grid
     build_grid(5, 0, maze_len)             # 1st argument = x value, 2nd argument = y value, 3rd argument = width of cell
     carve_out_maze(x,y)               # call build the maze  function
     plot_route_back(maze_len*maze_len, maze_len*maze_len)         # call the plot solution function
-    
+
     stop = timeit.default_timer()
-    comptime = round(stop - start,3) 
-    
+    comptime = round(stop - start,3)
+    print('computational time: {} seconds'.format(comptime))
+
     # ##### pygame loop #######
     running = True
     while running:
@@ -182,11 +184,10 @@ def make():
             # check for closing the window
             if event.type == pygame.QUIT:
                 running = False
-                
+
     pygame.display.quit()
     pygame.quit()
-    
-    print('computational time: {} seconds'.format(comptime))
+
     return comptime
 
 samples=10
